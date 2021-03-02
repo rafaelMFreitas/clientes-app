2ca5a26f1cfc1019fe3948fe580564aa77cc2a41
Rafael Moretti de Freitas <f594929@corp.caixa.gov.br>
2021-03-01 21:03:18 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { helloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    helloComponent
   ],
   imports: [
     BrowserModule,
