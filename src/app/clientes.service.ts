d620407dccb1b24752a3e55f4b38acaa9f14e756
Rafael Moretti de Freitas <f594929@corp.caixa.gov.br>
2021-03-14 15:19:29 -0300

    Estudo ate aula 72

---
+++ src/app/clientes.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Cliente } from './clientes/cliente';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor( private http : HttpClient ) {  }
+
+ listarClientes() : Observable<Cliente[]>{
+   return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
+ }
+
+  salvar(cliente : Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
+
+  obterClienteById(id : number) : Observable<Cliente>{
+    return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
+  }
+
+  atualizar(cliente : Cliente) : Observable<Cliente> {
+    return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
+  }
+}
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { ClientesService } from '../../clientes.service';
 import { Cliente } from '../cliente';
 
 @Component({
@@ -9,16 +11,48 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente
+  sucesso : boolean = false;
+  erros : String[];
+  id : number;
 
-  constructor() {
+  constructor(private service : ClientesService, private router : Router, private activatedRoute : ActivatedRoute) {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let params : Params = this.activatedRoute.params;
+    if(params && params.value && params.value.id) {
+      this.id = params.value.id;
+      this.service.obterClienteById(this.id)
+      .subscribe(
+        response => this.cliente = response,
+        errorResponse => this.cliente = new Cliente()
+      )
+    }
   }
 
   onSubmit() {
-    console.log(this.cliente);
-    
+    if(this.id) {
+      this.service.atualizar(this.cliente)
+      .subscribe( response => {
+        this.sucesso = true;
+        this.erros = [];
+      }, errorResponse => {
+        this.erros = ['Erro ao atualizar os cliente'];
+      })
+    } else {
+      this.service.salvar(this.cliente)
+      .subscribe( response => {
+        this.sucesso = true;
+        this.erros = [];
+        this.cliente = response;
+      }, errorResponse => {
+        this.erros = errorResponse.error.errors;
+      })
+    }
+  }
+
+  voltarParaListagem() {
+    this.router.navigate(['/clientes-lista']);
   }
 }
